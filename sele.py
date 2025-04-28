ee4fff40e45db2f7587a8923abdf9230fca56d36
shreya-mahajan1 <shreya.mhajn@gmail.com>
2025-04-28 08:29:16 +0530

    first commit

---
+++ sele.py
@@ -0,0 +1,8 @@
+import pytest 
+from selenium import webdriver 
+from selenium.webdriver.chrome.service import Service 
+from webdriver_manager.chrome import ChromeDriverManager 
+def test_open_site(): 
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install())) 
+    driver.get('https://selenium.dev/') 
+    driver.quit() 
